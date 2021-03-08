34e143b4e0d72b1306bf9a0e348e66dc234e81f9
Soultruster <79651387+Soultruster@users.noreply.github.com>
2021-03-09 00:06:56 +0200

    adaugat structuri

--- Source.c
+++ Source.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+struct album
+{
+    int codAlbum;
+    char titlu[20];
+    char data[10];
+    char copyWrite[20];
+    char format[20];
+};
+
+struct instrument
+{
+    int numarIdentificare;
+    char nume[20];
+    char cheieMuzicala[10];
+};
+
+struct muzician
+{
+    long long CNP;
+    char adresa[50];
+    long numarTelefon;
+    struct instrument instr;
+    struct album albm;
+};
